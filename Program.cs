25b3154ac1e77b29379b4ffd6c729bceb1049df9
GlossyCoin15 <91004595+jzp7326@users.noreply.github.com>
2022-08-23 16:37:02 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Peverly_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jake Peverly";
+            Console.WriteLine(name);
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine("10 % " + i.ToString() + " = " + (10 % i).ToString());
+            }
+        }
+    }
+}
+
+// Build Solution is F7.
+// Start Debugging is F5.
+// The program closed because it was done debugging.
+// Start without Debugging is Ctrl + F5
